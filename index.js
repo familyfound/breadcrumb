5a02a82988e93c04962dd29fc2d75adf0fb2fa89
Jared Forsyth <jared@jaredforsyth.com>
2013-07-06 10:20:01 -0600

    embedding images, + style updates

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 
 var angular = require('angularjs')
   , extend = require('extend')
+  , query = require('query')
+  , tip = require('tip')
 
   , template = require('./template');
+
+var helpText = "<b>Navigation History:</b> Keep track of how you got to this person.<br>" +
+  "The icons represent the relationship between people<br>" +
+  "<span class='uline'>Person</span> <i class='icon pedigree-up'></i> <span class='uline'>Ancestor</span><br>" +
+  "<span class='uline'>Person</span> <i class='icon pedigree-down'></i> <span class='uline'>Child</span><br>" +
+  "<span class='uline'>Person</span> <i class='icon pedigree-side'></i> <span class='uline'>Spouse</span><br>";
 
 angular.module('breadcrumb', [])
 
@@ -30,6 +38,12 @@ angular.module('breadcrumb', [])
         scope.$parent.$watch(name, function (value, old) {
           scope.history = value;
         });
+
+        // Status Help
+        var helpTip = new tip(helpText);
+        helpTip.attach(query('i.breadcrumb-help', element[0]));
+        helpTip.position('west');
+        helpTip.classname = 'breadcrumb-tip';
       }
     };
   });
